10683f58c034ac6e74f99bee93a510daaa2c7b02
jrbarrio <jorge.roldan@gmail.com>
2022-03-05 12:07:05 +0100

    Include handler binding in constructor to keep the scope of this

--- 01_day-1/03_event-handling-with-arrow-functions/src/components/Card.jsx
+++ 01_day-1/03_event-handling-with-arrow-functions/src/components/Card.jsx
@@ -7,31 +7,36 @@ class Card extends Component {
         text: 'Card text'
     };
 
-    // Use arrow functions in class components to keep the scope of 'this'
+//     constructor (props) {
+//         super(props);
+//         this.handleClick = this.handleClick.bind(this)
+//     }
+
+//     Use arrow functions in class components to keep the scope of 'this'. Constructor is not needed.
     handleClick = (event) => {
-        console.log("Arrow function", this); 
+        console.log("Arrow function", this);
         console.log(event);
         console.log(this.state.title);
         this.setState({ text: 'A different text' });
     }
 
-    // Does not work with <button onClick={ this.handleClick }>Change text!</button>
-    // Works with <button onClick={ event => this.handleClick(event) }>Change text!</button>
-    // handleClick(event) {
-    //     console.log("Function definition", this); 
-    //     console.log(event);
-    //     console.log(this.state.title);
-    //     this.setState({ text: 'A different text' });
-    // }
+//     Uncomment constructor to work with <button onClick={ this.handleClick }>Change text!</button>
+//     Also works with <button onClick={ event => this.handleClick(event) }>Change text!</button>
+//     handleClick(event) {
+//         console.log("Function definition", this);
+//         console.log(event);
+//         console.log(this.state.title);
+//         this.setState({ text: 'A different text' });
+//     }
     
-    // Does not work with <button onClick={ this.handleClick }>Change text!</button>
-    // Works with <button onClick={ event => this.handleClick(event) }>Change text!</button>
-    // handleClick = function(event) {
-    //     console.log("Function literal", this); 
-    //     console.log(event);
-    //     console.log(this.state.title);
-    //     this.setState({ text: 'A different text' });
-    // }
+//     Uncomment constructor to work with <button onClick={ this.handleClick }>Change text!</button>
+//     Also works with <button onClick={ event => this.handleClick(event) }>Change text!</button>
+//     handleClick = function(event) {
+//         console.log("Function literal", this);
+//         console.log(event);
+//         console.log(this.state.title);
+//         this.setState({ text: 'A different text' });
+//     }
     
     render() {
         return (<div className='card'>
